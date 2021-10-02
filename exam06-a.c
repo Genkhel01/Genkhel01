a9fd963190b9e497405959d0d4b4859646d0a2e2
Genkhel01 <genkhel@bk.ru>
2021-10-02 11:32:23 +0000

    Ia sdelat

---
+++ exam06-a.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#define days 30
+#define min 740
+#define max 780
+#define norm 760
+#define pressha[days]
+
+
+int main()
+{
+
+    for(int i = 0; i < days; i++)
+        pressha[i] = min + rand() % (max - min + 1)
+    for(int i = 0; i < days; i++)
+        printf("%d\t%d", i+1, pressha[i]);
+
+}
