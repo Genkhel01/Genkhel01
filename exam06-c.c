15d4e96b8f27b6611bfdbffbf77d49455463c374
Genkhel01 <genkhel@bk.ru>
2021-10-03 10:38:25 +0000

    exam06-c ready

--- exam06-c.c
+++ exam06-c.c
@@ -1,96 +1,87 @@
-#include <stdlib.h>
 #include <stdio.h>
-#include <linux/limits.h>
+#include <stdlib.h>
 
-#define min 740
-#define max 780
-#define norm 760
-#define days 31
-int press[31];
-
-int sortArray(int *num, int size)
+int daysSort(int *press, int *dayN, int length)
 {
-  for (int i = 0; i < size - 1; i++)
-  {
-    for (int j = size - 1; j > i; j--)
-    {
-      if (num[j - 1] > num[j])
-        {
-          int save = num[j-1];
-          num[j-1] = num[j];
-          num[j] = save;
-        }
-    }
-  }
+  for (int i = 0; i < length - 1; i++)
+    for (int j = length - 1; j > i; j--)
+      if (press[j - 1] > press[j])
+      {
+        int savePress = press[j-1];
+        press[j-1] = press[j];
+        press[j] = savePress;
+
+        int saveDate = dayN[j-1];
+        dayN[j-1] = dayN[j];
+        dayN[j] = saveDate;
+      }
 }
 
 int main()
 {
+  #define min 740
+  #define max 780
+  #define norm 760
+  #define days 31
+  int press[31];
+  int dayNum[31];
 
-  int daysBelow = 0;
-  int daysAbove = 0;
-  int daysExact = 0;
-  int dayDel;
-  scanf("%d", &dayDel);
-  dayDel--;
-
-  struct day
-  {
-    int Number[days];
-    int Press[days];
-    int Del[days];
-  };
+  //Scan dayDel
+    int dayDel;
+    printf("Day Delete: ");
+    scanf("%d", &dayDel);
+    dayDel--;
 
   //Fill days
-  for (int i = 0; i < days; i++)
-  {
-    day.Press[i] = min + rand() % (max - min + 1);
-  }
+    for (int i = 0; i < days; i++)
+    {
+      press[i] = min + rand() % (max - min + 1);
+      dayNum[i] = i + 1;
+      //Delete the day
+        if (i == dayDel)
+          press[i] = -1;
+    }
 
-  for (int i = 0; i < days; i++)
-  //Delete the day
-  if (i == dayDel)
-    day.Press[i] = -1;
-
-  //Sorting Array
-  sortArray(day.Press, days);
-
+  //Sort day's pressure
+    daysSort(press, dayNum, days);
 
   //Print days
-      printf("NumDay\t|Pressure\t|NumDay\t|Pressure\t|\n");
-  for (int i = 0; i < 15; i++)
-  {
-    if (i != dayDel)
-      printf("%d\t|\t%d\t|", i + 1, day.Press[i]);
-    else
-      printf("%d\t|\tdel\t|", i + 1);
-    if (i + 15 != dayDel)
-      printf("%d\t|\t%d\t|\n", i + 1 + 15, day.Press[i + 15]);
-    else
-      printf("%d\t|\tdel\t|\n", i + 1 + 15);
-  }
-  if (days > 30)
-    if (dayDel != 30)
-      printf(" \t|\t \t|%d\t|\t%d\t|\n", 30 + 1, day.Press[30]);
-    else
-      printf(" \t|\t \t|%d\t|\tdel\t|\n", 30 + 1);
+    printf("|NumDay\t|Pressure\t|NumDay\t|Pressure\t|\n");
+    for (int i = 0; i < 15; i++)
+    {
+      if (press[i] != -1)
+          printf("|%d\t|\t%d\t|", dayNum[i], press[i]);
+      else
+          printf("|%d\t|\tdel\t|", dayNum[i]);
+      if (press[i+15] != -1)
+          printf("%d\t|\t%d\t|\n", dayNum[i+15], press[i+15]);
+      else
+          printf("%d\t|\tdel\t|\n", dayNum[i+15]);
+    }
+    if (days > 30)
+      if (press[30] != -1)
+        printf(" \t|\t \t|%d\t|\t%d\t|\n", dayNum[30], press[30]);
+      else
+        printf(" \t|\t \t|%d\t|\tdel\t|\n", dayNum[30]);
 
   //Comparison
-  for (int i = 0; i < days; i++)
-    if (i != dayDel)
-  {
-    int v = day.Press[i];
-    if (v < norm)
-      daysBelow++;
-    else if (v > norm)
-      daysAbove++;
-    else
-      daysExact++;
-  }
-
-  printf("below:\t%d\n", daysBelow);
-  printf("above:\t%d\n", daysAbove);
-  printf("exact:\t%d\n", daysExact);
+    int daysBelow = 0;
+    int daysAbove = 0;
+    int daysExact = 0;
+    for (int i = 0; i < days; i++)
+      if (press[i] != -1)
+      {
+        int v = press[i];
+        if (v < norm)
+          daysBelow++;
+        else if (v > norm)
+          daysAbove++;
+        else
+          daysExact++;
+      }
+    printf("below:\t%d\n", daysBelow);
+    printf("above:\t%d\n", daysAbove);
+    printf("exact:\t%d\n", daysExact);
 
   return 0;
 }
