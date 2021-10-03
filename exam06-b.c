5a08aa33750cc3befe599aa4b48c1e1c0451c651
Genkhel01 <genkhel@bk.ru>
2021-10-03 10:35:12 +0000

    exam06-b ready

--- exam06-b.c
+++ exam06-b.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 #define min 740
-#define max 760
+#define max 780
 #define norm 760
 #define days 31
 int press[31];
@@ -14,6 +14,7 @@ int main()
   int daysAbove = 0;
   int daysExact = 0;
   int dayDel;
+  printf("Day Delete:\t");
   scanf("%d", &dayDel);
   dayDel--;
 
@@ -21,17 +22,33 @@ int main()
   for (int i = 0; i < days; i++)
   {
     press[i] = min + rand() % (max - min + 1);
-    //Delete the Day
+    //Delete the day
     if (i == dayDel)
       press[i] = -1;
   }
+
   //Print days
-      print("NumDay\t|\tPressure");
-  for (int i = 0; i < days; i++)
+  //int stolb = dayDel - 15;
+      printf("NumDay\t|Pressure\t|NumDay\t|Pressure\t|\n");
+  for (int i = 0; i < 15; i++)
+  {
     if (i != dayDel)
-      printf("%d\t|\t%d\n", i + 1, press[i]);
+      printf("%d\t|\t%d\t|", i + 1, press[i]);
+    else
+      printf("%d\t|\tdel\t|", i + 1);
+    if (i + 15 != dayDel)
+      printf("%d\t|\t%d\t|\n", i + 1 + 15, press[i + 15]);
+    else
+      printf("%d\t|\tdel\t|\n", i + 1 + 15);
+  }
+  if (days > 30)
+    if (dayDel != 30)
+      printf(" \t|\t \t|%d\t|\t%d\t|\n", 30 + 1, press[30]);
+    else
+      printf(" \t|\t \t|%d\t|\tdel\t|\n", 30 + 1);
 
   for (int i = 0; i < days; i++)
+    if (i != dayDel)
   {
     int v = press[i];
     if (v < norm)
