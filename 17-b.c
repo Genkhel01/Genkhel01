60002992de63546370bbef68a5573d202664a313
Genkhel01 <genkhel@bk.ru>
2021-10-13 07:27:08 +0000

    17 ready

---
+++ 17-b.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int main()
+{
+		char theString[256]; //create char array
+													//stdin - file systeam
+		fgets(theString, 256, stdin);  //read char from stream and store them into the array
+
+		//???
+		char *p = strchr(theString, '\n');
+			if (p != NULL) p[0] = '\0';
+
+		//the lenght of array in n
+		size_t lenghtOfTheStrig = strlen(theString);
+		//print the lenghtOfTheStrig
+		printf("lenght: %ld\n", lenghtOfTheStrig);
+
+		//counting the words
+		int numOfWords = 0;
+			for (int i = 0; i < lenghtOfTheStrig-1; i++)
+			{      //if the probel
+				 if (theString[i] == ' ' && theString[i+1] != ' ')
+				 {
+					 	numOfWords++;
+						//to uppercase (if not possible, it returns NULL)
+						theString[i+1] = toupper(theString[i+1]);
+				 }
+			}
+
+		//if there any symbol
+		    if(theString[0] == ' ') numOfWords--;
+			if(lenghtOfTheStrig > 0) numOfWords++;
+		printf("num words: %d\n", numOfWords);
+
+		theString[0] = toupper(theString[0]);
+		printf("%s \n", theString);
+
+		return 0;
+}
